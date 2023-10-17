f030d361ca7b0ec414447e1d4888b4a780482236
Rachmadan Nauval <rahmadannauvalhi@gmail.com>
2023-10-17 21:43:02 +0700

    Update bangun_datar3.php

--- bangun_datar3.php
+++ bangun_datar3.php
@@ -27,7 +27,84 @@
     </form>
 
     <?php
-    // Implementasi class BangunDatar, Persegi, PersegiPanjang, Segitiga, dan Lingkaran di sini (seperti yang disediakan sebelumnya).
+    // Implementasi class BangunDatar, Persegi, PersegiPanjang, Segitiga, dan Lingkaran
+    class BangunDatar {
+        public function hitungLuas() {
+            return "Hitung luas bangun datar.";
+        }
+    
+        public function hitungKeliling() {
+            return "Hitung keliling bangun datar.";
+        }
+    }
+    
+    class Persegi extends BangunDatar {
+        public $sisi;
+    
+        public function __construct($sisi) {
+            $this->sisi = $sisi;
+        }
+    
+        public function hitungLuas() {
+            return "Luas persegi: " . ($this->sisi * $this->sisi);
+        }
+    
+        public function hitungKeliling() {
+            return "Keliling persegi: " . (4 * $this->sisi);
+        }
+    }
+    
+    class PersegiPanjang extends BangunDatar {
+        public $panjang;
+        public $lebar;
+    
+        public function __construct($panjang, $lebar) {
+            $this->panjang = $panjang;
+            $this->lebar = $lebar;
+        }
+    
+        public function hitungLuas() {
+            return "Luas persegi panjang: " . ($this->panjang * $this->lebar);
+        }
+    
+        public function hitungKeliling() {
+            return "Keliling persegi panjang: " . (2 * ($this->panjang + $this->lebar));
+        }
+    }
+    
+    class Segitiga extends BangunDatar {
+        public $alas;
+        public $tinggi;
+    
+        public function __construct($alas, $tinggi) {
+            $this->alas = $alas;
+            $this->tinggi = $tinggi;
+        }
+    
+        public function hitungLuas() {
+            return "Luas segitiga: " . (0.5 * $this->alas * $this->tinggi);
+        }
+    
+        public function hitungKeliling() {
+            return "Hitung keliling segitiga.";
+        }
+    }
+    
+    class Lingkaran extends BangunDatar {
+        public $jariJari;
+    
+        public function __construct($jariJari) {
+            $this->jariJari = $jariJari;
+        }
+    
+        public function hitungLuas() {
+            return "Luas lingkaran: " . (3.14 * $this->jariJari * $this->jariJari);
+        }
+    
+        public function hitungKeliling() {
+            return "Keliling lingkaran: " . (2 * 3.14 * $this->jariJari);
+        }
+    }
     ?>
 
 </body>
